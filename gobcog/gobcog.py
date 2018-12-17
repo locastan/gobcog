45fff99f83bd25e6050ed81a7fcf72eaaad80348
locastan <inquisitor.czevak@gmail.com>
2018-12-17 20:02:39 +0100

    Pets in stats

--- gobcog/gobcog.py
+++ gobcog/gobcog.py
@@ -499,6 +499,8 @@ class GobCog(BaseCog):
         next_lvl = int((lvl+1) ** 4)
         if users[str(user.id)]['class'] != {} and 'name' in users[str(user.id)]['class']:
             clazz = users[str(user.id)]['class']['name'] + "\n" + users[str(user.id)]['class']['desc']
+            if users[str(user.id)]['class']['name'] == "Ranger" and 'pet' in users[str(user.id)]['class']['ability']:
+                clazz += "\n - Current pet: {}".format(users[str(member.id)]['class']['ability']['pet']['name'])
         else:
             clazz = "Hero."
         await ctx.send(
