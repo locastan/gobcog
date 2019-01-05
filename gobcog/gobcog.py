09555f3e21a79a014ca63482b42af78bdde90a3d
locastan <inquisitor.czevak@gmail.com>
2019-01-05 20:09:31 +0100

    Bugfix.

--- gobcog/gobcog.py
+++ gobcog/gobcog.py
@@ -683,7 +683,8 @@ class GobCog(BaseCog):
         global users
         for user in users:
             member = discord.utils.find(lambda m: m.id == user, ctx.guild.members)
-            await self.add_rewards(ctx, member, xp, cp, chests)
+            if member != None:
+                await self.add_rewards(ctx, member, xp, cp, chests)
         await ctx.send("All users were compensated with {} xp, {} cp and {} [normal, rare, epic] chests.".format(xp,cp,str(chests)))
 
     @commands.command(name="adventure", aliases=['a'])
