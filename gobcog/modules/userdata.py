3540a132ab8993052f14a07282eb5f4072b943a7
locastan <inquisitor.czevak@gmail.com>
2019-01-31 10:55:23 +0100

    check if userdata gets saved

--- gobcog/modules/userdata.py
+++ gobcog/modules/userdata.py
@@ -14,3 +14,4 @@ class Userdata:
     async def save():
         with Userdata.fp.open('w') as f:
             json.dump(Userdata.users, f, indent=4, default=lambda o: '<not serializable>', sort_keys=True)
+            print('Userdata saved.')
