861c36b1aba6feb05e01232f281cc299ca9f8203
locastan <inquisitor.czevak@gmail.com>
2019-09-06 13:12:41 +0200

    typo

--- gobcog/modules/quest.py
+++ gobcog/modules/quest.py
@@ -834,7 +834,7 @@ class Quest:
                     Userdata.users[str(member.id)]['class'].pop('basebonus')
             expired = []
             for buff in Userdata.users[str(member.id)]['buffs'].keys(): #reduce duration of active buffs
-                if buff != "rest:"
+                if buff != "rest":
                     if Userdata.users[str(member.id)]['buffs'][buff]['duration'] <= 1:
                         expired.append(buff)
                     else:
